49038a70e6fc92b44476763b595462ae3f5004d8
_NetNomad <43683711+TheNetNomad@users.noreply.github.com>
2022-08-31 00:24:18 -0400

    Update index.mjs
    -Single equal signs can now be used to test for equality only in IF statements
    -Fixed issue where VAL was returning NaN instead of 0 for non-numeric values
    -Fixed issue where semicolon terminating line would cause an abend
    -Fixed an issue where PROMPT and INPUT did not work when using GOSUB
    -Fixed bugs related to GOTO in IF statements

--- index.mjs
+++ index.mjs
@@ -42,7 +42,15 @@ math.import({
 	'COS':		function (input) {			return math.cos(input)},
 	'SIN':		function (input) {			return math.sin(input)},
 	'TAN':		function (input) {			return math.tan(input)},
-	'VAL':		function (input) {			return Number(input)},
+	'VAL':		function (input) {			
+		let x = Number(input);
+		if(!isNaN(x)){
+			return x;
+		}
+		else{
+			return 0;
+		}
+	},
 	'HEX$':		function (input) {			return math.hex(input).substring(2).toUpperCase()},
 	'OCT$':		function (input) {			return math.oct(input).substring(2).toUpperCase()},
 	'BIN$':		function (input) {			return math.bin(input).substring(2).toUpperCase()},
@@ -71,10 +79,12 @@ math.import({
 			textOut(input + "** **");
 			return 0;
 		}
+			
 		else{
 			promptLine = promptLine.toUpperCase();
+			console.log("VALUE RECIEVED " + promptLine);
 			if(promptLine == "END"){
-				currentLine = program.length ;
+				currentLine = program.length;
 				textOut("PROGRAM TERMINATED");
 			}
 			return promptLine;
@@ -118,12 +128,22 @@ function parse(line){
 		timeOut = true;
 		return;
 	}
-	
+
 	line = (line + "").trim();
+	
+	if(line.endsWith(";")){
+		//console.log("dog");
+		line = line.substring(0,line.length - 1);
+		console.log("Fixed line " + line);
+	}
+	
+	
 	let splitLine = line.split(/ (.*)/s);
 	
 	console.log("Evaluating " + line)
 
+	
+	
 	if((splitLine[0] != "IF" & isNaN(splitLine[0])) && (line.indexOf(":") > -1)){
 		let inQuote = false;
 		for (var i = 0; i < line.length; i++) {
@@ -177,18 +197,21 @@ function parse(line){
 		case "IF":
 			splitLine[1] = splitLine[1].replaceAll("THEN GOTO","THEN");
 			splitLine[1] = splitLine[1].replaceAll("ELSE GOTO","ELSE");
+			
 			if(splitLine[1].indexOf("ELSE") > -1){
 				splitLine[1] = splitLine[1].split(/(THEN)((?:\n|.)*)(ELSE)/);
 			}
-			else if(splitLine[1].indexOf("GOTO") > -1 && splitLine[1].indexOf("THEN" == -1)){
+			else if(splitLine[1].indexOf("GOTO") > -1 && splitLine[1].indexOf("THEN") == -1){
 				splitLine[1] = splitLine[1].split(/(GOTO)(.*)/s);
 			}
 			else{
 				splitLine[1] = splitLine[1].split(/(THEN)(.*)/s);
 			}
-			
+
 			try {
 				splitLine[1][0] = preparse(splitLine[1][0]);
+				splitLine[1][0] = splitLine[1][0].replaceAll(/(?<!(=|<|>))=(?!=)/g,"==");
+				console.log(splitLine[1][0] );
 				ifTest = basicEvaluate(splitLine[1][0],scope);
 			} catch (error) {
 				textOut(error.toString().split("\n")[0]   + " in " + currentLine);
@@ -233,19 +256,20 @@ function parse(line){
 			if(Number.isInteger(splitLine[1])){
 				stack.push(currentLine);
 				currentLine = parseInt(splitLine[1]);
-				//parse(program[currentLine]);
 				gosubret = false;
 				while(!gosubret && ((!quit && !wait) && currentLine < program.length)){
 	        console.log("Line " + currentLine + "outta" + program.length)
 					if(program[currentLine] != null){
 						parse(program[currentLine])
 					}
-					currentLine++;
-					if(currentLine >= program.length){
-						console.log("trying to END");
+					if(!wait){
+						currentLine++;
+					}
+					//if(currentLine >= program.length){
+						//console.log("trying to END");
 						//quit = true;
 						//return;
-					}
+					//}
 				}
 				
 			}
@@ -275,7 +299,6 @@ function parse(line){
 			}
 
 			if(foundComma){
-				//splitLine[1] = splitLine[1].split(",");
 				splitLine[1] = splitLine[1].split(/(,[^,]*)$/);
 			
 				if(splitLine[1][0] == ""){
@@ -307,6 +330,7 @@ function parse(line){
 			
 		
 		case "PRINT":
+			console.log(splitLine[1]);
 			try {
 				if(splitLine[1] == "" | splitLine[1] == null){
 					textOut(" ");
@@ -319,10 +343,11 @@ function parse(line){
 							inQuote = !inQuote
 						}
 						if(!inQuote && (splitLine[1].charAt(i) == ";" | splitLine[1].charAt(i) == ",")){
-							splitLine[1] = splitLine[1].substring(0,i) + "+" + splitLine[1].substring(i+1);
+							splitLine[1] = splitLine[1].substring(0,i) + ' + " " + ' + splitLine[1].substring(i+1);
 						}
 					}
-					
+
+					console.log(splitLine[1]);
 					output = basicEvaluate(splitLine[1],scope);
 					switch(math.typeOf(output)){
 						case "Unit":
@@ -348,7 +373,6 @@ function parse(line){
 		case "RUN":
 			currentLine = 0;
 			while((!quit && !wait) && currentLine < program.length){
-        //console.log("Line " + currentLine + "outta" + program.length)
 				if(program[currentLine] != null){
 					parse(program[currentLine])
 				}
@@ -392,7 +416,14 @@ function parse(line){
 		
 		default:
 			if(!isNaN(splitLine[0])){
-				program[parseInt(splitLine[0])] = splitLine[1]; 
+				if(currentLine == "terminal"){
+					program[parseInt(splitLine[0])] = splitLine[1]; 
+				}
+				else{
+					currentLine = splitLine[0];
+				}
+				
+				
 			}
 			else{
 				try {
